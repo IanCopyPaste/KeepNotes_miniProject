410ea717fb96d4a9ee3dc23509ead48f8f55225a
King Poison <adote.ronadrian.molleda@gmail.com>
2026-02-02 18:25:25 +0800

    first commit here, can delete can't edit

---
+++ script.js
@@ -0,0 +1,36 @@
+const btnClear = document.querySelector("#btnClear");
+const btnSave = document.querySelector("#btnSave");
+let txtMessage = document.querySelector("#txtMessage");
+let notesContainer = document.querySelector(".notes-container");
+
+btnClear.addEventListener("click",()=>{
+    txtMessage.value = "";
+});
+
+btnSave.addEventListener("click",()=>{
+    if(txtMessage.value === ""){
+        alert("Please enter a statement");
+        return;
+    }
+    const dotNote = document.createElement("div");
+    dotNote.className = "note"
+
+    const dotNoteContainer = document.createElement("div");
+    dotNoteContainer.className = "mainNote-container";
+
+    const pNote = document.createElement("p");
+    pNote.textContent = txtMessage.value;
+
+    const btnDel = document.createElement("button");
+    btnDel.textContent = "Delete"
+    btnDel.addEventListener("click",()=>{
+        dotNote.remove();
+    });
+
+    dotNoteContainer.appendChild(pNote);
+    dotNote.appendChild(dotNoteContainer);
+    dotNote.appendChild(btnDel);
+    notesContainer.appendChild(dotNote);
+
+    txtMessage.value = "";
+});
