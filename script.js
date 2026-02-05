b374d190061673f65613889adba97842ddf6a101
IanCopyPaste <adote.ronadrian.molleda@gmail.com>
2026-02-05 16:45:59 +0800

    added fetching text an html object

--- script.js
+++ script.js
@@ -3,27 +3,30 @@ const btnSave = document.querySelector("#btnSave");
 let txtMessage = document.querySelector("#txtMessage");
 let notesContainer = document.querySelector(".notes-container");
 
-btnClear.addEventListener("click",()=>{
+btnClear.addEventListener("click", () => {
     txtMessage.value = "";
 });
 
-btnSave.addEventListener("click",()=>{
-    if(txtMessage.value === ""){
+btnSave.addEventListener("click", () => {
+    if (txtMessage.value === "") {
         alert("Please enter a statement");
         return;
     }
+
     const dotNote = document.createElement("div");
-    dotNote.className = "note"
+    dotNote.className = "note";
 
     const dotNoteContainer = document.createElement("div");
     dotNoteContainer.className = "mainNote-container";
 
     const pNote = document.createElement("p");
+    pNote.className = "txtNote"
     pNote.textContent = txtMessage.value;
 
     const btnDel = document.createElement("button");
-    btnDel.textContent = "Delete"
-    btnDel.addEventListener("click",()=>{
+    btnDel.textContent = "Delete";
+    btnDel.addEventListener("click", event => {
+        event.stopPropagation();
         dotNote.remove();
     });
 
@@ -33,4 +36,10 @@ btnSave.addEventListener("click",()=>{
     notesContainer.appendChild(dotNote);
 
     txtMessage.value = "";
+});
+
+notesContainer.addEventListener("click", event => {
+    const note = event.target.closest(".note").querySelector(".txtNote");
+    if (!note) return;
+    alert(note.textContent);
 });
